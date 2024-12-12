2af076795169fcc574c1f64b65f86fbd4b114685
Gaonuk <rodrigogaonagonzalez@gmail.com>
2024-12-12 02:10:47 +0100

    feat: add environment variable for API_URL

--- app/api/generate-workout/route.ts
+++ app/api/generate-workout/route.ts
@@ -2,19 +2,18 @@ import { type NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+
 export async function POST(req: NextRequest) {
 	const body = await req.json();
 
-	const response = await fetch(
-		"http://localhost:8000/recommend-workout-formatted",
-		{
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(body),
+	const response = await fetch(`${API_URL}/recommend-workout-formatted`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
 		},
-	);
+		body: JSON.stringify(body),
+	});
 
 	const data = await response.json();
 
